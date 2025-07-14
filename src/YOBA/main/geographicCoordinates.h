f11428636d6bfc44f5024105d2ec43911494b702
Igor Timofeev <methodman1@mail.ru>
2025-07-14 21:19:00 +0300

    так, вот эт уже попизже формулки

--- src/YOBA/main/geographicCoordinates.h
+++ src/YOBA/main/geographicCoordinates.h
@@ -49,6 +49,25 @@ namespace YOBA {
 				return toCartesian(equatorialRadiusMeters);
 			}
 
+			static float getBearing(const float lat1, const float lon1, const float lat2, const float lon2) {
+				const auto lonDelta = lon2 - lon1;
+				const auto lat2Cos = std::cosf(lat2);
+
+				return std::atan2f(
+					std::sinf(lonDelta) * lat2Cos,
+					std::cosf(lat1) * std::sinf(lat2) - std::sinf(lat1) * lat2Cos * std::cosf(lonDelta)
+				);
+			}
+
+			static float getDistance(const float lat1, const float lon1, const float lat2, const float lon2) {
+				const auto latDeltaHalf = (lat2 - lat1) / 2;
+				const auto lonDeltaHalf = (lon2 - lon1) / 2;
+
+				const auto a = std::sinf(latDeltaHalf) * std::sinf(latDeltaHalf) + std::sinf(lonDeltaHalf) * std::sinf(lonDeltaHalf) * std::cosf(lat1) * std::cosf(lat2);
+
+				return equatorialRadiusMeters * std::atan2f(std::sqrtf(a), std::sqrtf(1 - a)) * 2;
+			}
+
 			float getLatitude() const {
 				return _latitude;
 			}
